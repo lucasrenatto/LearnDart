8c9f847a07721116d6bf2952ac57d26b1ecf473c
Lucas Renatto <lucasrenattotmz07@hotmail.com>
2024-06-26 22:56:23 -0300

    Implementação do projeto do curso 1 de flutter.

---
+++ alura/flutter/Desenvolva seu primeiro app com Flutter/alura_flutter_curso_1/README.md
@@ -0,0 +1,45 @@
+![Thumbnail GitHub](https://raw.githubusercontent.com/Kakomo/alura_flutter_curso_1/Aula_2/Readme%20Template.png)
+
+# Flutter: Crie seu primeiro app
+
+O Curso ensina o básico de Flutter para iniciantes que possuem apenas Dart como conhecimento prévio. Nesse curso estaremos explorando os conceitos de Widgets, Especificamente os Widgets Básicos do catalogo disponivel na documentação do flutter, assim como aprender o que é e como usar Stateless e Stateful Widgets.
+
+## 🔨 Projeto: Organizador de Tarefas
+
+O projeto do curso consiste em um organizador pessoal de tarefas, com gameficação. Cada tarefa adicionada tem um nível e uma dificuldade, conforme o usuário sobe de nível ele pode verificar através de uma barra de progresso se está perto de concluir a maestria na tarefa desejada.
+
+![](https://raw.githubusercontent.com/Kakomo/alura_flutter_curso_1/Aula_2/flutter1_gif.gif)
+
+## ✔️ Técnicas e tecnologias
+
+**Veja mais de perto o que você aprenderá sobre** :
+- `Widgets`: Elementos básicos do Flutter.
+- `Container`: Widget simples para desenhar na tela.
+- `Stack`: Widget que permite adicionar elementos um sobre o outro.
+- `Column`: Widget que permite adicionar elementos um ao lado do outro na vertical.
+- `Row`: Widget que permite adicionar elementos um ao lado do outro na horizonta.
+- `Text`: Widget que permite escrever textos na tela.
+- `ElevatedButton`: Widget com função de botão, que permite ações no projeto.
+- `Scaffold`: Widget que usa o Material Design.
+- `Stateless Widget`: Template de Widget para criar nossos próprios Widgets estáticos.
+- `Stateful Widget`: Template de Widget para criar nossos próprios Widgets dinâmicos.
+- `Image`: Widget que permite adicionar imagens ao projeto.
+- `AnimatedOpacity`: Widget animado que muda a opacidade dos elementos no projeto.
+- `Refatoração` : Conceito para organizar e melhorar o nosso projeto para exibir aos seus colegas!
+
+ 
+
+
+## 🛠️ Abrir e rodar o projeto
+
+**Para executar este projeto você precisa:**
+
+- Ter uma IDE, que pode ser o  [Android Studio](https://developer.android.com/) instalado na sua máquina
+- Ter a [SDK do Flutter](https://docs.flutter.dev/get-started/install) na versão 3.0.0
+
+
+## 📚 Mais informações do curso
+
+Gostou do projeto e quer conhecer mais? Você pode [acessar o curso](https://cursos.alura.com.br/course/flutter-widgets-stateless-stateful-imagens-animacoes) que desenvolve o projeto desde o começo!
+
+Esse curso faz parte da [formação de Flutter da Alura](https://cursos.alura.com.br/formacao-flutter)
---
+++ alura/flutter/Desenvolva seu primeiro app com Flutter/alura_flutter_curso_1/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter: Primeiros Passos'),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 100,
+                width: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.pinkAccent,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                    height: 50,
+                    width: 50,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: const Text(
+                  'Diamante Amarelo', // palavra aleatoria
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('Você apertou o Botão!');
+                },
+                child: const Text('Aperte o botão!!'),
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: Colors.black87),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Você apertou o FAB!');
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
